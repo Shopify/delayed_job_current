0060a352289bd90c20ddd51bf42f44539c34d98b
keith@dailymugshot.com <keith@dailymugshot.com>
2010-04-26 22:05:04 -0400

    happy tests are passing tests

--- spec/worker_spec.rb
+++ spec/worker_spec.rb
@@ -1,5 +1,9 @@
 require File.dirname(__FILE__) + '/database'
 
+class SimpleJob
+  def perform; puts "I'm being run"; end
+end
+
 describe Delayed::Worker do
   before do
     Delayed::Job.delete_all
@@ -15,18 +19,36 @@ describe Delayed::Worker do
     w.idle_after.should == 2
   end
   
-  it "should initialize with next_idle = Time.new + idle_after" do
-    w = Delayed::Worker.new
-    w.next_idle.should > Time.new + w.idle_after.minute - 2
-    w.next_idle.should < Time.new + w.idle_after.minute + 2
+  it "should start with next_idle = Time.new + idle_after" do
+    worker = Delayed::Worker.new
+    worker_thread = Thread.new { worker.start }
+    expected_next_idle = Time.new + worker.idle_after.minute
+    next_idle = worker.next_idle
+    sleep(2)
+    Thread.kill(worker_thread)
+    next_idle.should be_close(expected_next_idle, 2)
   end
   
   it "should update next_idle after we return from Delayed::Job.work_off" do
-    
+    worker = Delayed::Worker.new
+    worker_thread = Thread.new { worker.start }
+    next_idle_start = worker.next_idle
+    sleep(2)
+    Delayed::Job.enqueue SimpleJob.new
+    sleep(10)
+    next_idle_after = worker.next_idle
+    Thread.kill(worker_thread)
+    next_idle_after.should > next_idle_start
   end
   
   it "should call the on_idle method if Time.new > next_idle" do
-    
+    Delayed::Worker.class_eval "def on_idle; @test_var = true; end"
+    worker = Delayed::Worker.new
+    worker_thread = Thread.new { worker.start }
+    worker.instance_variable_defined?(:@test_var).should == false
+    sleep(70)
+    worker.instance_variable_defined?(:@test_var).should == true
+    Thread.kill(worker_thread)
   end
   
 end
