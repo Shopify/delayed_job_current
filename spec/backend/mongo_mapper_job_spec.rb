f6f02b61b4e0e3e6629aa8a997a9885aa0e12a3a
Brandon Keepers <brandon@collectiveidea.com>
2010-11-13 13:32:44 -0600

    fix mongomapper specs

--- spec/backend/mongo_mapper_job_spec.rb
+++ spec/backend/mongo_mapper_job_spec.rb
@@ -6,11 +6,11 @@ describe Delayed::Backend::MongoMapper::Job do
   before(:all) do
     @backend = Delayed::Backend::MongoMapper::Job
   end
-  
+
   before(:each) do
     MongoMapper.database.collections.each(&:remove)
   end
-  
+
   it_should_behave_like 'a backend'
 
   describe "indexes" do
@@ -22,23 +22,23 @@ describe Delayed::Backend::MongoMapper::Job do
       @backend.collection.index_information.detect { |index| index[0] == 'locked_by_1' }.should_not be_nil
     end
   end
-  
+
   describe "delayed method" do
     class MongoStoryReader
       def read(story)
         "Epilog: #{story.tell}"
       end
     end
-    
+
     class MongoStory
       include ::MongoMapper::Document
       key :text, String
-      
+
       def tell
         text
       end
     end
-    
+
     it "should ignore not found errors because they are permanent" do
       story = MongoStory.create :text => 'Once upon a time...'
       job = story.send_later(:tell)
@@ -51,7 +51,7 @@ describe Delayed::Backend::MongoMapper::Job do
       job = story.send_later(:tell)
 
       job.payload_object.class.should   == Delayed::PerformableMethod
-      job.payload_object.object.should  == "LOAD;MongoStory;#{story.id}"
+      job.payload_object.object.should  == story
       job.payload_object.method.should  == :tell
       job.payload_object.args.should    == []
       job.payload_object.perform.should == 'Once upon a time...'
@@ -62,16 +62,16 @@ describe Delayed::Backend::MongoMapper::Job do
       job = MongoStoryReader.new.send_later(:read, story)
       job.payload_object.class.should   == Delayed::PerformableMethod
       job.payload_object.method.should  == :read
-      job.payload_object.args.should    == ["LOAD;MongoStory;#{story.id}"]
+      job.payload_object.args.should    == [story]
       job.payload_object.perform.should == 'Epilog: Once upon a time...'
     end
   end
-  
+
   describe "before_fork" do
     after do
       MongoMapper.connection.connect_to_master
     end
-    
+
     it "should disconnect" do
       lambda do
         Delayed::Backend::MongoMapper::Job.before_fork
@@ -83,12 +83,12 @@ describe Delayed::Backend::MongoMapper::Job do
     before do
       MongoMapper.connection.close
     end
-    
+
     it "should call reconnect" do
       lambda do
         Delayed::Backend::MongoMapper::Job.after_fork
       end.should change { !!MongoMapper.connection.connected? }.from(false).to(true)
     end
   end
-  
+
 end
